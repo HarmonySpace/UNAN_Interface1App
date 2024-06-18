9dfc300a14a64dd82101d49c713c96dfe24c39db
HarmonySpace <jcspace@gmail.com>
2024-06-18 15:12:16 -0600

    update controller
    add create method
    add edit method
    add routes

--- routes.php
+++ routes.php
@@ -3,6 +3,7 @@
 $routes = function (FastRoute\RouteCollector $r) {
     $r->addRoute('GET', '/', 'Controller::index');
     $r->addRoute('GET', '/get', 'Controller::get');
+    $r->addRoute('GET', '/create', 'Controller::create');
     $r->addRoute('POST', '/store', 'Controller::store');
     $r->addRoute('GET', '/edit/{id}', 'Controller::edit');
 };
--- src/Controller.php
+++ src/Controller.php
@@ -24,6 +24,7 @@ class Controller
         $this->dir = __DIR__ . '/..';
         $this->json_file = $this->dir . "/assets/file.json";
     }
+    // views
     //route "/"
     public function index()
     {
@@ -34,6 +35,27 @@ class Controller
             echo "File not found: $view_path";
         }
     }
+    //route "/create"
+    public function create()
+    {
+        $view_path = $this->dir . "/src/views/create.html";
+        if (file_exists($view_path)) {
+            require_once $view_path;
+        } else {
+            echo "File not found: $view_path";
+        }
+    }
+    //route "/create"
+    public function edit($id)
+    {
+        $view_path = $this->dir . "/src/views/edit.html";
+        if (file_exists($view_path)) {
+            require_once $view_path;
+        } else {
+            echo "File not found: $view_path";
+        }
+    }
+    //data functions
     //route "/get"
     public function get()
     {
@@ -42,6 +64,7 @@ class Controller
         header('Content-Type: application/json');
         echo $json_response;
     }
+    //route "/store"
     public function store()
     {
         date_default_timezone_set('America/Managua');
